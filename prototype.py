cdb2d689d1f6fe724583e993d51536cc24483a23
potatoe88888 <86220169+potatoe88888@users.noreply.github.com>
2021-12-07 22:54:29 -0800

    Create prototype.py
    here is some code that seems to do what is needed for the application. Maybe want to make a nice gui for the user. Maybe want to scrap the youtube video links from the output urls.

---
+++ prototype.py
@@ -0,0 +1,100 @@
+### SOURCES
+# https://www.dataquest.io/blog/last-fm-api-python/
+# https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary-exercise-49.php
+# https://stackoverflow.com/questions/44720682/does-the-lastfm-api-provide-youtube-links
+
+import requests
+import json
+
+API_KEY = '12a9ed2e87c6978ad67c7e07792f1622'
+USER_AGENT = 'bayera88888'
+ARTIST_LIMIT = 20
+TOP_TRACKS_LIMIT = 5
+
+def lastfm_getData(payload):
+    # use a header for last.fm to not block the request [https://www.last.fm/api/intro]
+    headers = {'user-agent': USER_AGENT}
+    url = 'https://ws.audioscrobbler.com/2.0/'
+
+    # build payload
+    payload['api_key'] = API_KEY
+    payload['format'] = 'json'
+
+    response = requests.get(url, headers=headers, params=payload)
+    return response
+
+def get_similarArtists(userInput_artist):
+    # capture similar artists and urls
+    similar_artist_array = []
+    top_tracks_urls = []
+
+    # build out payload -- artist name
+    request_sa = lastfm_getData({
+        'method': 'artist.getSimilar',
+        'limit': ARTIST_LIMIT,
+        'autocorrect': 1,                           # allows for mis-spells, decently works too!
+        'artist': userInput_artist
+    })
+
+    for index_sa in range(0, ARTIST_LIMIT):
+        # capture artist name
+        response_name_display = request_sa.json()['similarartists']['artist'][index_sa]['name']
+
+        # build out array -- artist name
+        similar_artist_array.append(response_name_display)
+
+        # build out array -- urls
+        top_tracks_urls.append(get_topTracks(response_name_display))
+
+    print("similar_artist_array ::")
+    print(similar_artist_array)
+    print("\n==============================\n")
+    print("top_tracks_urls ::")
+    print(top_tracks_urls)
+
+    return None
+
+def get_topTracks(response_name_display):
+    top_tracks_array = []
+
+    for index in range(0, TOP_TRACKS_LIMIT):
+        # build out payload -- urls
+        request_tt = lastfm_getData({
+            'method': 'artist.getTopTracks',
+            'limit': TOP_TRACKS_LIMIT,
+            'page': 1,
+            'autocorrect': 1,                        # allows for mis-spells, decently works too!
+            'artist': response_name_display
+        })
+        # capture TOP_TRACKS_LIMIT amount of urls
+        response_url_display = request_tt.json()['toptracks']['track'][index]['url']
+
+        # build out array
+        top_tracks_array.append(response_url_display)
+
+    return top_tracks_array
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+# display all information
+#jprint(r3.json())
+
+def check_connections():
+    # check connection set up -- expect '200'
+    r = lastfm_getData({
+        'method': 'artist.getSimilar',
+        'limit': ARTIST_LIMIT,
+        'autocorrect': 1,  # allows for mis-spells, decently works too!
+        'artist': 'Cher'
+    })
+
+    return r.status_code
+
+# quick check of set up
+print(check_connections())
+
+# example use case
+get_similarArtists("Cold Play")
